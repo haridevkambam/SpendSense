bcd30ecb9fa42b04557c7d7c9114b231d4daab68
haridevkambam <haridevkambam@gmail.com>
2025-01-11 00:41:34 +0530

    create user and transaction schema

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { connectDB } from "./db/connectDB.js";
+dotenv.config();
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -31,7 +34,9 @@ app.use(
 );
 
 // Modified server startup
-await new Promise ((resolve) =>
+await new Promise((resolve) =>
     httpServer.listen({ port: 4000 }, resolve),
-  );
+);
+
+await connectDB();
 console.log(`🚀 Server ready at http://localhost:4000/`);
