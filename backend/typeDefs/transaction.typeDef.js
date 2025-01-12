85e978a57179b4c5bb93c54c4b61e91cfaca73c2
haridevkambam <haridevkambam@gmail.com>
2025-01-12 23:29:43 +0530

    Add transaction resolvers and type definitions

--- backend/typeDefs/transaction.typeDef.js
+++ backend/typeDefs/transaction.typeDef.js
@@ -13,6 +13,7 @@ const transactionTypeDef = `#graphql
     type Query {
         transactions: [Transaction!]
         transaction(transactionId: ID!): Transaction
+        # TODO => ADD CATEGORY STATISTICS QUERY
     }
 
     type Mutation {
